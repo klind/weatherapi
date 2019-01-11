c516ed75a6695530925b9f811a33cf295fea306c
lind <klpcrap@gmail.com>
2019-01-11 09:24:01 -0800

    Updated synchronized scope as weatherResponseCache.put was outside scope

--- src/main/java/com/code/weatherapi/client/decorator/OpenWeatherAppCache.java
+++ src/main/java/com/code/weatherapi/client/decorator/OpenWeatherAppCache.java
@@ -14,25 +14,24 @@ import java.time.LocalDateTime;
 @Component
 @Primary
 public class OpenWeatherAppCache extends OpenWeatherAppDecorator {
-  private static Logger log = LoggerFactory.getLogger(OpenWeatherAppCache.class);
-  private WeatherResponseCache weatherResponseCache;
+	private static Logger log = LoggerFactory.getLogger(OpenWeatherAppCache.class);
+	private WeatherResponseCache weatherResponseCache;
 
-  public OpenWeatherAppCache(OpenWeatherAppClient openWeatherAppClient) {
-    super(openWeatherAppClient);
-    weatherResponseCache = WeatherResponseCache.getInstance();
-  }
+	public OpenWeatherAppCache(OpenWeatherAppClient openWeatherAppClient) {
+		super(openWeatherAppClient);
+		weatherResponseCache = WeatherResponseCache.getInstance();
+	}
 
-  @Override
-  public WeatherResponse getWind(String zip) {
-    synchronized (weatherResponseCache) {
-      if (weatherResponseCache.containsKey(zip)) {
-        return weatherResponseCache.get(zip).getWeatherResponse();
-      }
-    }
-    WeatherResponse weatherResponse = super.getWind(zip);
-    weatherResponseCache.put(zip, new CacheWeatherResponse(LocalDateTime.now(), weatherResponse));
-    log.debug("Adding {} to the cache. New size is {}", weatherResponse,
-        weatherResponseCache.size());
-    return weatherResponse;
-  }
+	@Override
+	public WeatherResponse getWind(String zip) {
+		synchronized (weatherResponseCache) {
+			if (weatherResponseCache.containsKey(zip)) {
+				return weatherResponseCache.get(zip).getWeatherResponse();
+			}
+			WeatherResponse weatherResponse = super.getWind(zip);
+			weatherResponseCache.put(zip, new CacheWeatherResponse(LocalDateTime.now(), weatherResponse));
+			log.debug("Adding {} to the cache. New size is {}", weatherResponse, weatherResponseCache.size());
+			return weatherResponse;
+		}
+	}
 }
